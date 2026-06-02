ffcb2d5047f309493afd5ae8aedfc220dc2aa3af
Dener Chaves <denerdeveloper26@gmail.com>
2026-06-02 07:26:55 -0300

    Script para extração de dados em PDF

---
+++ main.py
@@ -0,0 +1,15 @@
+import cv2
+import pytesseract
+
+
+#Ler imgagem
+imagem = cv2.imread('comprovante_pix.jpeg')
+
+#Estamos definindo o caminho do tesseract para o pytesseract, para que ele possa usar o OCR para extrair texto da imagem.
+caminho = r"C:\Users\dener\AppData\Local\Programs\Tesseract-OCR\tesseract.exe"
+
+pytesseract.pytesseract.tesseract_cmd = caminho
+#Pedir pytessact Estrair texto da imgagem 
+texto = pytesseract.image_to_string(imagem)
+
+print(texto) 
